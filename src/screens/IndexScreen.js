bea37c360c6fbdf156b87ec0f1c3b6a48258e350
geomukkath <geomukkath@yandex.com>
2022-10-02 10:20:55 +0530

    Added navigation packages to the app

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
