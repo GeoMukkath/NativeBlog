9ac061b58c2fe09a4594fdf76048e3a34cab4576
GeoMukkath <geomukkath@yandex.com>
2022-10-14 17:29:13 +0530

    Added create screen option

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import { BlogProvider } from "./src/context/BlogContext";
 import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import { Feather } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,8 +16,23 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Blogs" component={IndexScreen} />
+        <Stack.Screen
+          name="Blogs"
+          component={IndexScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <TouchableOpacity>
+                <Feather
+                  name="plus"
+                  size={47}
+                  onPress={() => navigation.navigate("Create")}
+                />
+              </TouchableOpacity>
+            ),
+          })}
+        />
         <Stack.Screen name="Show" component={ShowScreen} />
+        <Stack.Screen name="Create" component={CreateScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
