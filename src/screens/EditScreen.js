96f894db879396bbb81bec036b49c24bff59e87c
GeoMukkath <geomukkath@yandex.com>
2022-10-18 12:42:15 +0530

    Added sample edit screen for testing

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,26 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput } from "react-native";
+import React, { useState, useContext } from "react";
+import BlogContext from "../context/BlogContext";
 
 const EditScreen = ({ navigation, route }) => {
+  const { data } = useContext(BlogContext);
+  const blogPost = data.find((blogPost) => blogPost.id === route.params.id);
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>EditScreen - {route.params.id}</Text>
+      <Text>Enter Title </Text>
+      <TextInput
+        value={title}
+        onChangeText={(newTitle) => setTitle(newTitle)}
+      />
+      <Text>Enter Content</Text>
+      <TextInput
+        value={content}
+        onChangeText={(newContent) => setContent(newContent)}
+      />
     </View>
   );
 };
