9f20946499d44eb0962648ee557478b3bdf3e429
GeoMukkath <geomukkath@yandex.com>
2022-10-25 10:58:43 +0530

    Added demo of app

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://b1c1-2401-4900-2341-7873-84a4-6f86-daef-3ef2.in.ngrok.io",
+  baseURL: "https://1497-223-30-70-211.in.ngrok.io",
 });
